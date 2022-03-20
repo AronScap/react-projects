a7abee8b52deea786eeef57e181c43df66bdafef
Aron Selhorst <contato@insource.com.br>
2022-03-19 22:44:25 -0300

    commit on 'main'

--- pokemon/pokemon-app/src/App.js
+++ pokemon/pokemon-app/src/App.js
@@ -1,9 +1,35 @@
+import React, { useEffect, useState } from 'react'; 
 import './App.css';
+import PokeApi from './PokeApi';
+import Pagination from './components/Pagination';
 
 function App() {
+  const [pokemonList, setPokemonList] = useState([]);
+  useEffect(()=>{
+    const loadAll = async () => {
+      let list = await PokeApi.getPokemonsList();
+      // console.log(list)
+      setPokemonList(list);
+    }
+    loadAll();
+  }, []);
+
   return (
     <div className="App">
-      Hello Pokemon
+      {/* header */}
+      Hello Pokemons
+      {pokemonList.results.length > 0 && 
+        <section className='pokemon--list'>
+          {pokemonList.results.map( (item,key) => (
+            <div className='pokemon--info' key={key}>
+              <h1>{item.name}</h1>
+              <a>Ver mais</a>
+            </div>
+          ))}
+        </section>
+      }
+      <Pagination />
+      {/* footer */}
     </div>
   );
 }
---
+++ pokemon/pokemon-app/src/PokeApi/index.js
@@ -0,0 +1,21 @@
+const API_base = "https://pokeapi.co/api/v2/";
+
+const pokemonFetch = async (endpoint) => {
+    const request = await fetch(`${API_base}${endpoint}`);
+    const requestJson = await request.json();
+    console.log(requestJson);
+    return requestJson;
+}
+
+export default{
+    getPokemonsList: async (limit,offset) => {
+        return await pokemonFetch(`pokemon/?limit=${limit}&offset=${offset}`);
+    },
+    getPokemonInfo: async (pokemonID) => {
+        let info = {};
+        if(pokemonID){
+            info = await pokemonFetch(`pokemon/${pokemonID}`);
+        }
+        return info;
+    }
+}
